94a3dbf18597dc5edfe9895d342d7ce9d76331b3
aur4or4a <aur4or4a@gmail.com>
2026-02-21 17:51:16 +0530

    ss

---
+++ test.js
@@ -0,0 +1,6 @@
+function actionman(){let dd=prompt('What is 9/11?');
+    let ddd=document.getElementById("lulu_birthday").value;
+    console.log(ddd);
+
+    if(dd==="IDK"&&ddd==="1396-06-08"){ alert("Congatulation roach on this endavor.");}
+}
