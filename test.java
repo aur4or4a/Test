2dd6fb6d67ec16e2f42228f0a3ea6411874861fc
aur4or4a <aur4or4a@gmail.com>
2026-02-21 21:24:26 +0530

    New Season
    Ok, start for a fresh test.

--- test.java
+++ test.java
@@ -1,2 +1,7 @@
 import java.io.*;
 import java.net.*;
+public class test{
+
+public static void main(String[] Zerif){
+    
+}}
--- test.js
+++
@@ -1,6 +0,0 @@
-function actionman(){let dd=prompt('What is 9/11?');
-    let ddd=document.getElementById("lulu_birthday").value;
-    console.log(ddd);
-
-    if(dd==="IDK"&&ddd==="1396-06-08"){ alert("Congatulation roach on this endavor.");}
-}
