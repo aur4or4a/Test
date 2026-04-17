98cbe5b85e80845b58e26b9b529af9bc45f7993d
Lucky Aurora <aur4or4a@gmail.com>
2026-04-17 17:40:30 +0530

    Update display.php

--- display.php
+++ display.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
+<html>
+<head>
+<title>Display Form Data</title>
+</head>
 <body>
-<h2> Fuck You Again!</h2>
-  
-  <?
-
+<h2>Submitted Data</h2>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$name = $_POST['name'];
+$email = $_POST['email'];
+$course = $_POST['course'];
+echo "Name: " . $name . "<br>";
+echo "Email: " . $email . "<br>";
+echo "Course: " . $course . "<br>";
+}
 ?>
 </body>
 </html>
